b1f1169ac7c184384bf3f915d163a77928bf11ee
Jakub Narloch <jmnarloch@gmail.com>
2020-02-05 01:02:28 -0800

    Added LICENSE and initial documentation

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
-# aws-eventbridge-pattern-builder
+#  AWS EventBridge Rule Pattern DSL
+
+> A Java utility library for creating EventBridge Patterns.
+
+## Setup
+ 
+Add the library to your project:
+ 
+```xml
+ <dependency>
+   <groupId>io.jmnarloch</groupId>
+   <artifactId>aws-eventbridge-pattern-builder</artifactId>
+   <version>1.0.0</version>
+ </dependency>
+```
+
+## Usage
+
+Library defines very simple builder pattern for creating an EventBridge Rule patterns.
+
+Examples:
+
+Matching events publish by `aws.ec2`.
+
+```
+EventsPattern.builder()
+        .equal("source", "aws.ec2")
+        .build();
+```
+
+Matching events based on the `detail` content.
+
+```
+EventsPattern.builder()
+    .path("detail")
+        .equal("state", "terminated")
+        .parent()
+    .build();
+```
+
+Matching attributes by prefix.
+
+```
+EventsPattern.builder()
+    .prefix("region", "eu-")
+    .build();
+```
+
+Matching events that attribute is any value other then specified.
+
+```
+EventsPattern.builder()
+    .path("detail")
+        .anythingBut("state", "initializing")
+        .parent()
+    .build();
+```
+
+Matching the events based on specified number comparision.
+
+```
+EventsPattern.builder()
+    .path("detail")
+        .numeric("c-count", Comparision.GREATER_THAN, 5)
+        .numeric("d-count", Comparision.LOWER_THAN, 10)
+        .numeric("x-limit", Comparision.EQUAL, 3.018e2)
+        .parent()
+    .build();
+``` 
+
+See also the [documentation](https://docs.aws.amazon.com/eventbridge/latest/userguide/content-filtering-with-event-patterns.html#filtering-numeric-matching) for supported EventBridge operators.
+
+## License
+
+Apache 2.0
--- src/test/java/io/jmnarloch/aws/events/pattern/Demo.java
+++ src/test/java/io/jmnarloch/aws/events/pattern/Demo.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2020 the original author or authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.jmnarloch.aws.events.pattern;
 
 import io.jmnarloch.aws.events.matchers.Comparision;
@@ -21,8 +36,8 @@ class Demo {
                 .equal("source", "aws.ec2")
                 .equal("detail-type", "EC2 Instance State-change Notification")
                 .path("detail")
-                    .equal("state", "terminated")
-                    .parent()
+                .equal("state", "terminated")
+                .parent()
                 .build();
 
         EventsPattern.builder()
@@ -35,27 +50,27 @@ class Demo {
 
         EventsPattern.builder()
                 .equal("source", "aws.autoscaling")
-                .prefix("time", "eu-")
+                .prefix("region", "eu-")
                 .build();
 
         EventsPattern.builder()
                 .path("detail")
-                    .anythingBut("state", "initializing")
-                    .parent()
+                .anythingBut("state", "initializing")
+                .parent()
                 .build();
 
         EventsPattern.builder()
                 .path("detail")
-                    .numeric("c-count", Comparision.GREATER_THAN, 5)
-                    .numeric("d-count", Comparision.LOWER_THAN, 10)
-                    .numeric("x-limit", Comparision.EQUAL, 3.018e2)
-                    .parent()
+                .numeric("c-count", Comparision.GREATER_THAN, 5)
+                .numeric("d-count", Comparision.LOWER_THAN, 10)
+                .numeric("x-limit", Comparision.EQUAL, 3.018e2)
+                .parent()
                 .build();
 
         EventsPattern.builder()
                 .path("detail")
-                    .notExists("c-count")
-                    .parent()
+                .notExists("c-count")
+                .parent()
                 .build();
     }
 }
